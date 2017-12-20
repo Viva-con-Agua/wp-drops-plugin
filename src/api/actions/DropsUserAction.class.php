c00fea333da9c686f99fab9358a890342283538c
Tobias Kaestle <t.kaestle@vivaconagua.org>
2017-12-20 16:40:46 +0100

    dbg

--- src/api/actions/DropsUserAction.class.php
+++ src/api/actions/DropsUserAction.class.php
@@ -58,11 +58,10 @@ abstract class DropsUserAction
         $restClient = new RestClient($options);
         $response = $restClient->post(get_option('dropsActionUrl'));
 
+        var_dump($response);
+        die();
         if ($response->info->http_code == 200) {
 
-            var_dump(
-                $response->response
-            );
             return (new DropsResponse())
                 ->setCode($response->info->http_code)
                 ->setContext(__CLASS__)
