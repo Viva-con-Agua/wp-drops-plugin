6b555494625802031a4f418700d29934c3107ddb
Tobias Kaestle <t.kaestle@vivaconagua.org>
2017-12-20 15:56:05 +0100

    read function from get, remove backslashes

--- src/api/server/DropsUserController.class.php
+++ src/api/server/DropsUserController.class.php
@@ -16,18 +16,16 @@ class DropsUserController extends DropsController
      */
     public function run()
     {
-
         if (!$this->isRequestValid()) {
             return;
         }
 
-        $parameter = $this->getParameter(DropsSessionController::DROPSFNC, $_POST);
+        $parameter = $this->getParameter(DropsSessionController::DROPSFNC, $_GET);
 
         switch ($parameter) {
             case self::CREATE:
 
                 $userData = $this->getUserData();
-
                 $dataHandler = new DropsUserDataHandler();
                 $userReceiver = new DropsUserCreator($userData);
                 $userReceiver->setDataHandler($dataHandler);
@@ -86,6 +84,7 @@ class DropsUserController extends DropsController
     private function getUserData()
     {
         $userData = $_POST['user'];
+        $userData = str_replace('\\', '', $userData);
         return json_decode($userData, true);
     }
 
