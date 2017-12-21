9205ec77d82832f373777683c31cf59b342bccc8
Tobias Kaestle <t.kaestle@vivaconagua.org>
2017-12-21 16:27:36 +0100

    added different urls and types

---
+++ src/api/actions/DropsUserProfileReader.class.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once 'DropsUserAction.class.php';
+
+/**
+ * Class DropsUserReader
+ * The class reads the user from drops
+ */
+class DropsUserProfileReader extends DropsUserAction
+{
+
+    const ACTION_TYPE = 'READ';
+
+    /**
+     * Creates an array with the userdata
+     * @param int $userId
+     * @return array
+     */
+    protected function createUserData($userId)
+    {
+        return array();
+    }
+
+    /**
+     * Returns the action to add it to the parameters
+     */
+    protected function getAction()
+    {
+        return self::ACTION_TYPE;
+    }
+
+    /**
+     * Returns the actionUrl
+     */
+    protected function getActionUrl()
+    {
+        return get_option('dropsUserProfileUrl');
+    }
+
+    /**
+     * Returns the action to add it to the parameters
+     */
+    protected function getActionType()
+    {
+        return parent::ACTIONTYPE_GET;
+    }
+
+}
