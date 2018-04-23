e8103eda419704e10be81cad4119d64e2aebd410
Tobias Kaestle <t.kaestle@vivaconagua.org>
2018-04-23 14:00:57 +0200

    added threading for nats listening

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ require_once 'src/api/actions/DropsUserImageUpdater.class.php';
 require_once 'src/api/actions/DropsUserProfileReader.class.php';
 require_once 'src/api/actions/DropsUserDeleter.class.php';
 require_once 'src/api/DropsLoginHandler.class.php';
-require_once 'src/api/DropsLogoutProcess.class.php';
+//require_once 'src/api/DropsLogoutProcess.class.php';
 require_once 'src/api/DropsLogoutHandler.class.php';
 
 require_once 'src/DropsSessionDataHandler.class.php';
