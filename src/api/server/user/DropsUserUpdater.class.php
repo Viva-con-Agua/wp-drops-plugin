e3409fba544e2aa7ef022e39db1a2aefde4f0ef9
Tobias Kaestle <t.kaestle@vivaconagua.org>
2018-12-10 18:24:50 +0100

    map capabilities

--- src/api/server/user/DropsUserUpdater.class.php
+++ src/api/server/user/DropsUserUpdater.class.php
@@ -18,7 +18,7 @@ class DropsUserUpdater
 
     private $requiredUserData = array('uuid');
     private $optionalUserFields = array('user_login', 'user_nicename', 'user_email', 'display_name');
-    private $optionalUserMetaFields = array('nickname', 'first_name', 'mail_switch', 'pool_lang', 'secondary_nl', 'last_name', 'capabilites', 'mobile', 'residence', 'birthday', 'gender', 'nation', 'city');
+    private $optionalUserMetaFields = array('nickname', 'first_name', 'mail_switch', 'pool_lang', 'secondary_nl', 'last_name', 'wp_capabilites', 'mobile', 'residence', 'birthday', 'gender', 'nation', 'city');
 
     /**
      * @var UserDataHandlerInterface $dataHandler
