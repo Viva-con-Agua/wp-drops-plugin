f43480398192fcbf48b1382bcff6a4f7b86a0572
Tobias Kaestle <t.kaestle@vivaconagua.org>
2018-12-19 12:18:38 +0100

    enable api

--- index.php
+++ index.php
@@ -54,7 +54,8 @@ function handleDropsLogin() {
 		if (strpos($_SERVER['REQUEST_URI'], 'datenschutz') !== false
 			|| strpos($_SERVER['REQUEST_URI'], 'nutzungs') !== false
 			|| strpos($_SERVER['REQUEST_URI'], 'rausloggen') !== false
-			|| strpos($_SERVER['REQUEST_URI'], 'faq') !== false) {
+			|| strpos($_SERVER['REQUEST_URI'], 'faq') !== false
+			|| isset($_GET['api'])) {
 				return;
 			}
 		
