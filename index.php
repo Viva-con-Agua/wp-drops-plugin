27c7b25c76a8e803ca9488d93cf454e90bda4ddb
Tobias Kaestle <t.kaestle@vivaconagua.org>
2019-01-10 23:08:15 +0100

    updated redirection

--- index.php
+++ index.php
@@ -63,7 +63,6 @@ function handleDropsLogin() {
 
             // we have got the sessions, destroy them all!
             $sessions->destroy_all();
-			die();
 			
         } else {
 			$dataHandler->updateExpiryDate(get_current_user_id());
