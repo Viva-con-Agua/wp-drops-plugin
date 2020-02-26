7aa656ac5b561e7750773620b79a792d0188f84c
Tobi Ka <t.kaestle@vivaconagua.org>
2020-02-26 19:30:22 +0100

    added debug

---
+++ src/api/server/DropsNewsletterController.class.php
@@ -0,0 +1,85 @@
+<?php
+
+require_once 'DropsController.class.php';
+require_once 'user/DropsUserCreator.class.php';
+require_once 'newsletter/DropsNewsletterReader.class.php';
+
+
+/**
+ * Class DropsUserController
+ */
+class DropsNewsletterController extends DropsController
+{
+	
+    /** Path to post userdata to, to update a user in the good ol' pool */
+    const UPDATE = 'update';
+	
+    /** Path to post userdata to, to create a user in the good ol' pool */
+    const CREATE = 'create';
+
+    /** Path to post userdata to, to logout a user from the good ol' pool */
+    const LOGOUT = 'logout';
+
+    /** Path to post userdata to, to logout a user from the good ol' pool */
+    const REMOVE = 'delete';
+	
+    /** Path to post userdata to, to logout a user from the good ol' pool */
+    const READ = 'read';
+
+    /**
+     * Checks if the parameters are valid and calls the user creation action
+     */
+    public function run()
+    {
+		
+		$requestValidation = $this->isRequestValid();
+
+        if (!empty($requestValidation)) {
+            $response = (new DropsResponse())->setCode(400)->setMessage('Invalid request! Please check your data and format! Message: ' . $requestValidation)->setContext(__CLASS__);
+			self::logResponse($response);
+            return $response->getFormat(DropsResponse::JSON);
+        }
+		
+        switch ($this->apiFunction) {    
+			case self::READ:
+
+                $data = $this->getData();
+                $dataHandler = new DropsUserDataHandler();
+                $newsletterReader = new DropsNewsletterReader($data);
+                $newsletterReader->setDataHandler($dataHandler);
+                $response = $userReader->run();
+
+                self::logResponse($response);
+
+                echo $response->getFormat(DropsResponse::JSON);
+                exit;
+
+                break;
+            default:
+				$response = (new DropsResponse())->setCode(400)->setMessage('API Function not implemented!')->setContext(__CLASS__);
+				self::logResponse($response);
+                break;
+        }
+
+    }
+
+    /**
+     * @return bool
+     */
+    private function isRequestValid()
+    {
+        if ($_SERVER['REQUEST_METHOD'] != 'GET') {
+            return 'Wrong request method given!';
+        }
+
+        return '';
+
+    }
+
+    private function getData()
+    {
+		// DATAJSON
+		return $this->data;
+    }
+
+}
