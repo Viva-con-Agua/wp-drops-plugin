908f07002917985257a96f2be0c15fed6d48b48c
Tobi Ka <t.kaestle@vivaconagua.org>
2020-02-26 19:25:25 +0100

    added debug

--- src/api/server/DropsAPIController.class.php
+++ src/api/server/DropsAPIController.class.php
@@ -33,7 +33,7 @@ class DropsAPIController extends DropsController
 		//$this->createReceivedDataFromArray($_POST);	
 		$actionCall = $this->getParameter('action', $_GET);	
 	
-		if (!$this->isValid() && $actionCall != self::NEWSLETTER) {
+		if (!$this->isValid() && $apiCall != self::NEWSLETTER) {
 			die();
 			return;
 		}
